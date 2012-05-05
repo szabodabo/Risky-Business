875012bdaa0232166e1f0d30dfabcdc0518230db
Dakota <szabod@rpi.edu>
2012-05-05 00:28:33 -0400

    Added more logic, but hot-potato seems to be broken.

--- risk.c
+++ risk.c
@@ -10,7 +10,10 @@
 #define PREV_RANK_FROM(R) ((R == 0) ? commSize-1 : R-1)
 #define RECV 0
 #define SEND 1
-
+#define HEADS 1
+#define TAILS 2
+#define COIN_FLIP 2
+#define ACC(A, R, C) A[tt_total * R + C]
 
 #include "risk_input.h"
 
@@ -24,7 +27,16 @@ typedef enum answer_t {
 
 int diceRoll( int sides ) {
 	int randInt = rand() % sides;
+	//printf("DiceRoll: %d\n", randInt+1);
 	return randInt + 1;
+}
+
+int do_battle(int teamA, int teamB)
+{
+	int a_roll = rand() % (teamA + 1); // 0 to 5
+	int b_roll = rand() % (teamB + 1); // 0 to 4
+	
+	return a_roll - b_roll;
 }
 
 //GIANT IDEA:
@@ -66,7 +78,7 @@ void apply_strategy(int territory, int tt_total, int *troopCounts, int *teamIDs,
 
 int main( int argc, char **argv ) {
 	int myRank, commSize;
-	int i, j;
+	int i, j, k;
 
 	srand(2); //BIG COMMENT
 
@@ -77,9 +89,11 @@ int main( int argc, char **argv ) {
 	//Everyone has their own slice of these arrays
 	int **adjMatrix; //Adjacency matrix
 	int **edgeActivity; //Edge data (passed around)
+	int **myBattleFlips;
 
-	MPI_Datatype MATRIX_ROW;
+	//MPI_Datatype MATRIX_ROW;
 	MPI_Request *requests;
+	MPI_Status *statuses;
 
 	MPI_Init( &argc, &argv );
 	MPI_Comm_rank( MPI_COMM_WORLD, &myRank );
@@ -95,46 +109,44 @@ int main( int argc, char **argv ) {
 		read_header_info( &tt_total );
 	}
 
-	MPI_Type_contiguous( tt_total, MPI_INT, &MATRIX_ROW );
+	//MPI_Type_contiguous( tt_total, MPI_INT, &MATRIX_ROW );
 
 	typedef struct battle_calc_msg_t {
 		int source_rank;
 		int row_offset;
-		int *adjSlice[tt_total];
-		int *edgeSlice[tt_total];
-		int *adjCalculated[tt_total];
-		int *edgeResult[tt_total];
+		int *adjSlice;
+		int *edgeSlice;
+		int *battleFlip;
+		int *edgeResult;
 	} BATTLE_CALC_MSG;
 
-	int SLICE_SIZE;
-	MPI_Type_size( MATRIX_ROW, &SLICE_SIZE );
-
+	int SLICE_LEN = tt_per_rank * tt_total;
 
 	int BCMSG_LENGTHS[6] = {
 		1,
 		1,
-		tt_per_rank,
-		tt_per_rank,
-		tt_per_rank,
-		tt_per_rank
+		SLICE_LEN,
+		SLICE_LEN,
+		SLICE_LEN,
+		SLICE_LEN
 	};
 
 	MPI_Aint BCMSG_OFFSETS[6] = {
 		0,
 		sizeof(int), //Size of first element...
 		2 * sizeof(int), //... + Size of second element...
-		2 * sizeof(int) + tt_per_rank * SLICE_SIZE,
-		2 * sizeof(int) + 2 * tt_per_rank * SLICE_SIZE,
-		2 * sizeof(int) + 3 * tt_per_rank * SLICE_SIZE
+		2 * sizeof(int) + SLICE_LEN * sizeof(int),
+		2 * sizeof(int) + 2 * SLICE_LEN * sizeof(int),
+		2 * sizeof(int) + 3 * SLICE_LEN * sizeof(int)
 	};
 
 	MPI_Datatype BCMSG_TYPES[6] = {
 		MPI_INT,
 		MPI_INT,
-		MATRIX_ROW,
-		MATRIX_ROW,
-		MATRIX_ROW,
-		MATRIX_ROW
+		MPI_INT,
+		MPI_INT,
+		MPI_INT,
+		MPI_INT
 	};
 
 	MPI_Datatype MPI_BCMSG;
@@ -151,16 +163,20 @@ int main( int argc, char **argv ) {
 	tt_per_rank = tt_total / commSize;
 
 	requests = calloc( 2, sizeof(MPI_Request) );
+	statuses = calloc( 2, sizeof(MPI_Status) );
 	
 	troopCounts = calloc( tt_total, sizeof(int) );
 	teamIDs = calloc ( tt_total, sizeof(int) );
 
 	edgeActivity = calloc( tt_per_rank, sizeof(int *) );
 	adjMatrix = calloc( tt_per_rank, sizeof(int *) );
+	myBattleFlips = calloc( tt_per_rank, sizeof(int *) );
+
 
 	for (i = 0; i < tt_per_rank; i++) {
 		edgeActivity[i] = calloc( tt_total, sizeof(int) );
 		adjMatrix[i] = calloc( tt_total, sizeof(int) );
+		myBattleFlips[i] = calloc( tt_total, sizeof(int) );
 	}
 
 	//Temp variables to enable all-reduce (might use these later when communicating further results)	
@@ -205,6 +221,7 @@ int main( int argc, char **argv ) {
 		apply_strategy( tt_offset+i, tt_total, troopCounts, teamIDs, adjMatrix[i], edgeActivity[i] );
 	}
 
+/*
 	MPI_Barrier(MPI_COMM_WORLD);
 	sleep(1);
 	if (myRank == 0) {
@@ -218,14 +235,14 @@ int main( int argc, char **argv ) {
 		//printf("Territories bordered by territory %d:\n", territoryID);
 		for ( j = 0; j < tt_total; j++ ) {
 			if ( adjMatrix[i][j] == 1 ) {
-				printf("\t%d -- %d", territoryID, j);
+				printf("\t\"%d (%d)\" -- \"%d (%d)\"", territoryID, troopCounts[territoryID], j, troopCounts[j]);
 				int numToPrint = edgeActivity[i][j];
 				int posFlag = (numToPrint > 0);
 				if (posFlag == 1) {
-					printf("\t[taillabel = \"%d\" fontcolor = \"red\"]\n", numToPrint);
+					printf(" [taillabel = \"%d\" fontcolor = \"red\"]\n", numToPrint);
 				} else {
 					numToPrint *= -1;
-					printf("\t[taillabel = \"%d\" fontcolor = \"blue\"]\n", numToPrint);
+					printf(" [taillabel = \"%d\" fontcolor = \"blue\"]\n", numToPrint);
 				}
 			}
 		}
@@ -236,50 +253,148 @@ int main( int argc, char **argv ) {
 	if (myRank == 0) {
 		printf("\tsep = 1\n\toverlap = false\n\tsplines = true\n}\n");
 	}
+	*/
 
 	MPI_Barrier( MPI_COMM_WORLD );
 
 	printf("[%d] Beginning result exchange\n", myRank);
-	//Do math for each battle to find the winner and how many troops the winner has left
 	BATTLE_CALC_MSG *recvBuffer = calloc( 1, sizeof(BATTLE_CALC_MSG) );
 	BATTLE_CALC_MSG *sendBuffer = calloc( 1, sizeof(BATTLE_CALC_MSG) );
-	for ( i = 0; i < tt_per_rank; i++ ) {
-		recvBuffer->adjSlice[i] = calloc( tt_total, sizeof(int) );
-		recvBuffer->edgeSlice[i] = calloc( tt_total, sizeof(int) );
-		recvBuffer->adjCalculated[i] = calloc( tt_total, sizeof(int) );
-		recvBuffer->edgeResult[i] = calloc( tt_total, sizeof(int) );
 
-		sendBuffer->adjSlice[i] = calloc( tt_total, sizeof(int) );
-		sendBuffer->edgeSlice[i] = calloc( tt_total, sizeof(int) );
-		sendBuffer->adjCalculated[i] = calloc( tt_total, sizeof(int) );
-		sendBuffer->edgeResult[i] = calloc( tt_total, sizeof(int) );
+	//Allocate memory for send/receive buffers
+	for ( i = 0; i < tt_per_rank; i++ ) {
+		recvBuffer->adjSlice = calloc( tt_total * tt_per_rank, sizeof(int) );
+		recvBuffer->edgeSlice = calloc( tt_total * tt_per_rank, sizeof(int) );
+		recvBuffer->battleFlip = calloc( tt_total * tt_per_rank, sizeof(int) );
+		recvBuffer->edgeResult = calloc( tt_total * tt_per_rank, sizeof(int) );
+
+		sendBuffer->adjSlice = calloc( tt_total * tt_per_rank, sizeof(int) );
+		sendBuffer->edgeSlice = calloc( tt_total * tt_per_rank, sizeof(int) );
+		sendBuffer->battleFlip = calloc( tt_total * tt_per_rank, sizeof(int) );
+		sendBuffer->edgeResult = calloc( tt_total * tt_per_rank, sizeof(int) );
 	}
 
 	recvBuffer->source_rank = myRank;
 	recvBuffer->row_offset = tt_offset;
 
-	for ( j = 0; j < tt_per_rank; j++ ) {
-		memcpy( sendBuffer->adjSlice[j], adjMatrix[j], tt_total * sizeof(int) );
-		memcpy( sendBuffer->edgeSlice[j], edgeActivity[j], tt_total * sizeof(int) );
-	}
-	printf("[%d] RecvBuffer Populated!\n", myRank);
+	for ( i = 0; i < tt_per_rank; i++ ) {
+		for ( j = 0; j < tt_total; j++ ) {
+			
+			/* ==================== COIN FLIP ===================
+			To decide which territory's rank computes the result on an edge:
 
-	for ( i = 0; i < commSize; i++ ) {
+			If the flips are the same, the territory with the lower ID 
+			  is responsible for the calculation.
+			If the flips are different, the terrirory with the higher ID
+			  is responsible for the calculation.
+			This should evenly distribute the work among the ranks and should
+			  solve the lower-IDed-territories-have-more-work problem. 
+			There might be a more efficient way to to this, 
+			  but this seemed pretty good when I was working on it.  -DJS    */
+
+			myBattleFlips[i][j] = diceRoll(COIN_FLIP);
+			//printf("[%d] MyBattleFlips[%d][%d]: %d\n", myRank, i, j, myBattleFlips[i][j]);
+		}
+	}
+
+	for ( j = 0; j < tt_per_rank; j++ ) {
+		memcpy( recvBuffer->adjSlice + j * tt_total, adjMatrix[j], tt_total * sizeof(int) );
+		memcpy( recvBuffer->edgeSlice + j * tt_total, edgeActivity[j], tt_total * sizeof(int) );
+		memcpy( recvBuffer->battleFlip + j * tt_total, myBattleFlips[j], tt_total * sizeof(int) );
+	}
+
+	/*for ( i = 0; i < tt_per_rank; i++ ) {
+		for ( j = 0; j < tt_total; j++ ) {
+			
+		}
+	}*/
+
+	//printf("[%d] RecvBuffer Populated!\n", myRank);
+
+	for ( i = 0; i < commSize; i++ ) { // I -> iteration of MPI hot-potato
+
+		for ( j = 0; j < tt_per_rank; j++ ) {
+			for ( k = 0; k < tt_total; k++ ) {
+				printf("[%d] (T %d -- T %d ) RecvBufAdjSlice[%d][%d] = %d\n", myRank, j+recvBuffer->row_offset, k, j, k, ACC(recvBuffer->adjSlice, j, k));
+			}
+		}
+
+		for ( j = 0; j < tt_per_rank; j++ ) { // J -> my territories
+			for ( k = 0; k < tt_per_rank; k++ ) { // K -> territories in currently received slice
+
+				//Don't even bother if we're on the diagonal
+				if ( tt_offset+j == recvBuffer->row_offset+k ) { continue; }
+
+				//printf("[%d] RecvBattleFlips[%d][%d]: %d\n", myRank, i, j, recvBuffer->battleFlip[k][j]);
+
+				printf("[%d] I [terr %d] flipped %d; Foe [terr %d] (rank %d) flipped %d\n", myRank, tt_offset+j, myBattleFlips[ j ][ recvBuffer->row_offset+k ], recvBuffer->row_offset+k, recvBuffer->source_rank, ACC(recvBuffer->battleFlip, k, tt_offset+j) );
+				char sameFlip = ACC(recvBuffer->battleFlip, k, tt_offset+j) == myBattleFlips[j][recvBuffer->row_offset+k];
+				char iAmHigher = tt_offset+j > recvBuffer->row_offset+k;
+				char isMyJob;
+				if (sameFlip == 1) {
+					if (iAmHigher == 0) {
+						isMyJob = 1;
+					} else {
+						isMyJob = 0;
+					}
+				} else { //Different Flips
+					if (iAmHigher == 1) {
+						isMyJob = 1;
+					} else {
+						isMyJob = 0;
+					}
+				}
+				printf("[%d] J: %d; J-Offset: %d; K: %d\n", myRank, j, tt_offset, k);
+				printf( "[%d] My Job: %d; Borders: %d\n", myRank, isMyJob, ACC(recvBuffer->adjSlice, k, tt_offset+j) );
+
+				//If there's an adjacency, I'll decide the battle if it's my job to do so
+				if ( ACC(recvBuffer->adjSlice, k, tt_offset+j) == 1 && isMyJob == 1 ) {
+					
+					int myNode_power = edgeActivity[ j ][ recvBuffer->row_offset+k ];
+					int otherNode_power = ACC(recvBuffer->edgeSlice, k, tt_offset+j);
+
+					int result = do_battle( myNode_power, otherNode_power );
+
+					//IF RESULT < 0:
+					// otherNode won (with *-result* troops surviving the attack)
+					// but otherNode will see this as a win, so we'll store
+					// it as a positive value (negating it)
+					//IF RESULT > 0:
+					// myNode won (with *result* troops surviving the attack)
+					// but this is the otherNode's slot, so we'll negate it
+					ACC(recvBuffer->edgeResult, k, tt_offset+j) = -1 * result;
+					printf("[%d] Battle between MyNode %d and OtherNode %d: %d\n", myRank, tt_offset+j, recvBuffer->row_offset+k, result);					
+				}
+			}
+		}
+
 		printf("[%d] Boxing up rank %d's data...\n", myRank, recvBuffer->source_rank);
-		//Assume information we've just finished working on is in recvBuffer.
+		//Information we've just finished working on is in recvBuffer.
 		//Move the received info to the send buffer.
 		sendBuffer->source_rank = recvBuffer->source_rank;
 		sendBuffer->row_offset = recvBuffer->row_offset;
 
 		for ( j = 0; j < tt_per_rank; j++ ) {
-			memcpy( sendBuffer->adjSlice[j], recvBuffer->adjSlice[j], tt_total * sizeof(int) );
-			memcpy( sendBuffer->edgeSlice[j], recvBuffer->edgeSlice[j], tt_total * sizeof(int) );
-			memcpy( sendBuffer->adjCalculated[j], recvBuffer->adjCalculated[j], tt_total * sizeof(int) );
-			memcpy( sendBuffer->edgeResult[j], recvBuffer->edgeResult[j], tt_total * sizeof(int) );
+			memcpy( sendBuffer->adjSlice, recvBuffer->adjSlice, tt_total * tt_per_rank * sizeof(int) );
+			memcpy( sendBuffer->edgeSlice, recvBuffer->edgeSlice, tt_total * tt_per_rank * sizeof(int) );
+			memcpy( sendBuffer->battleFlip, recvBuffer->battleFlip, tt_total * tt_per_rank * sizeof(int) );
+			memcpy( sendBuffer->edgeResult, recvBuffer->edgeResult, tt_total * tt_per_rank * sizeof(int) );
 		}
 		MPI_Irecv( recvBuffer, 1, MPI_BCMSG, PREV_RANK_FROM(myRank), 0, MPI_COMM_WORLD, &requests[RECV] );
 		MPI_Isend( sendBuffer, 1, MPI_BCMSG, NEXT_RANK_FROM(myRank), 0, MPI_COMM_WORLD, &requests[SEND] );
-		MPI_Waitall( 2, requests, MPI_STATUSES_IGNORE );
+		MPI_Waitall( 2, requests, statuses );
+		for ( j = 0; j < 2; j++ ) {
+			printf("[%d] J=%d, Error = %d\n", myRank, j, statuses[j].MPI_ERROR);
+		}
+
+		printf("[%d] Completed Irecv of data from rank %d\n", myRank, recvBuffer->source_rank);
+
+
+		for ( j = 0; j < tt_per_rank; j++ ) {
+			for ( k = 0; k < tt_total; k++ ) {
+				printf("[%d] (T %d -- T %d ) RecvBufAdjSlice[%d][%d] = %d\n", myRank, j+recvBuffer->row_offset, k, j, k, ACC(recvBuffer->adjSlice, j, k));
+			}
+		}
 	}
 
 
