5fb2c8318f578da6f6782457fbc39afd39ed9aea
Dakota <szabod@rpi.edu>
2012-05-06 18:25:46 -0400

    Graphviz generation stuff

--- extras/to_graphviz.c
+++ extras/to_graphviz.c
@@ -13,20 +13,51 @@ int main( int argc, char **argv ) {
 		exit( EXIT_FAILURE );
 	}
 
-	int rc, n, i;
+	int i, j;
 
 	char *fileName = argv[1];
 	FILE *file;
 
-	file = fopen( fileName, 'rb' );
+	file = fopen( fileName, "rb" );
 
 	int numTerritories;
 
-	n = fread( &numTerritories, sizeof(int), 1, file );
+	printf("graph G {\n");
+
+	fread( &numTerritories, sizeof(int), 1, file );
 
 	for ( i = 0; i < numTerritories; i++ ) {
-		int 
+		int terrNum, totalCount, teamNum;
+
+		fread( &terrNum, sizeof(int), 1, file );
+		fread( &totalCount, sizeof(int), 1, file );
+		fread( &teamNum, sizeof(int), 1, file );
+
+		printf("\t%d [label = \"%d{%d} (%d)\"]\n",
+			terrNum, terrNum, teamNum, totalCount);
+
+		for ( j = 0; j < numTerritories; j++ ) {
+			int border; //0 -> No; 1 -> Yes
+			int numTroopsOnEdge; //How many troops stationed?
+			int action; //0 -> Def; 1 -> Att
+
+			fread( &border, sizeof(int), 1, file );
+			fread( &numTroopsOnEdge, sizeof(int), 1, file );
+			fread( &action, sizeof(int), 1, file );
+			//"9{9} (0)" -- "0{0} (273)" [taillabel = "0" fontcolor = "blue"]
+			if ( action == 0 ) {
+				printf("\"\t\t%d -- %d [taillabel = \"%d\" fontcolor = \"blue\"]\n",
+					i, j, numTroopsOnEdge);
+			} else {
+				printf("\"\t\t%d -- %d [taillabel = \"%d\" fontcolor = \"red\"]\n",
+					i, j, numTroopsOnEdge);			
+			}
+		}
 	}
+	
+	printf("\tsep = 1\n\toverlap = false\n\tsplines = true\n");
+
+	printf("}\n");
 
 	return EXIT_SUCCESS;
 }
