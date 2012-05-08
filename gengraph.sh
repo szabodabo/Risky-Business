033956fe84914c85ed212d63bb3a96d7c7f3270a
Dakota <szabod@rpi.edu>
2012-05-08 05:52:48 -0400

    Changeeeeeees

--- gengraph.sh
+++ gengraph.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#
+#for NODES in 16 32 64 128 256 512 1024 2048 4096 8192; do
+#	bin/gen complete $NODES 10000 input/graphbin$NODES
+#done
 
-for NODES in 16 32 64 128 256 512 1024 2048 4096 8192; do
-	bin/gen complete $NODES 10000 input/graphbin$NODES
-done
+bin/gen complete 10080 10000 input/graphbin
