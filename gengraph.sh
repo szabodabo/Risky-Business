0e0ff2dce2f32d193eb4ffba09ac2a100d03f0b9
nmzimmt@gmail.com <nmzimmt@gmail.com>
2012-05-08 06:28:39 -0400

    more changes

--- gengraph.sh
+++ gengraph.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#
-#for NODES in 16 32 64 128 256 512 1024 2048 4096 8192; do
-#	bin/gen complete $NODES 10000 input/graphbin$NODES
-#done
 
-bin/gen complete 10080 10000 input/graphbin
+for NODES in 16 32 64 128 256 512 1024 2048 4096 8192; do
+	bin/gen star $NODES 10000 input/graphbin$NODES
+done
