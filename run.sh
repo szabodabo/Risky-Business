81bc684c7f9565c540ef2f7ceae9c7c1c7fe5b46
nmzimmt@gmail.com <nmzimmt@gmail.com>
2012-05-08 00:00:00 -0400

    First results batch

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 rm -f input/graphbin
+clear
+make clean
+make kratos
+./gengraph.sh
 
 GRAPHS=`ls -l input | awk '{print $9; }' | grep graphbin`
 
 for FILE in $GRAPHS; do
+	rm -f graphs/graphout*
 	cp input/$FILE input/graphbin
 	mpirun -np 16 bin/risk --max-iterations=10
 done
